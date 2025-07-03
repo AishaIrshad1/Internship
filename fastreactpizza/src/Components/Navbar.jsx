0839c40d6a39f73fb6d722164e2c548d432a2539
AishaIrshad1 <aishakaimkhani153@gmail.com>
2025-07-03 20:46:17 +0500

    fix: corrected Footer import case for Vercel

--- fastreactpizza/src/Components/Navbar.jsx
+++ fastreactpizza/src/Components/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import { useUser } from "../context/usercontext";
 
 export default function Navbar() {
@@ -8,13 +7,13 @@ export default function Navbar() {
   return (
     <div className="flex justify-between items-center bg-yellow-400 p-3">
       <div className="text-2xl">
-        <p>Fast React Pizza Co.</p>
+        <p>Logo</p>
       </div>
 
       <div>
         <input
           type="text"
-          placeholder="Search order #"
+          placeholder="search"
           className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
         />
       </div>
